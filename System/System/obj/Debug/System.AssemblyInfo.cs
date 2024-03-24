39f1d7664ae7b49d6bed903280a5bb3dae26fbd1
CreeperGuy72192 <65190117+CreeperGuy72192@users.noreply.github.com>
2024-03-24 00:44:02 -0400

    Fixed ISO creation.. it should be bootable now

--- System/System/obj/Debug/System.AssemblyInfo.cs
+++ System/System/obj/Debug/System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11191d9b622bfa8b425ce4e63fb8d82fcb320122")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6294cf0244cfec8fe09a336625cbaedbf338ba16")]
 [assembly: System.Reflection.AssemblyProductAttribute("System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 cd System/System.x86
 dotnet build
 cd bin
-dotnet Tools/Mosa.Tool.Launcher.Console.dll -oMax -launch-off -vmware-svga System.x86.dll
+dotnet Tools/Mosa.Tool.Launcher.Console.dll -oMax -launch-off System.x86.dll
 cd ../../..
 
 cp /var/folders/6g/cn2k0n6n6mxf063hwd89rbch0000gn/T/MOSA/System.x86.img ISO/CD_root/images
@@ -9,5 +9,7 @@ cp /var/folders/6g/cn2k0n6n6mxf063hwd89rbch0000gn/T/MOSA/System.x86.img ISO/CD_r
 mv ISO/CD_root/images/System.x86.img ISO/CD_root/images/System.img
 
 mkisofs -o ISO/System.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table ISO/CD_root
+
+perl ISO/isohybrid.pl ISO/System.iso
 
 qemu-system-x86_64 -cdrom ISO/System.iso
