4332c5896a7c8f660f4efae1472b3001c38e5d58
CreeperGuy72192 <65190117+CreeperGuy72192@users.noreply.github.com>
2024-03-24 07:11:54 -0400

    Fixed the build script

--- System/System/obj/Debug/System.AssemblyInfo.cs
+++ System/System/obj/Debug/System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+371bcae3f573a09f2ae25546aa75107ae8f56f1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed9ee40b71819a28a8172e4433a20be6a6f69890")]
 [assembly: System.Reflection.AssemblyProductAttribute("System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
