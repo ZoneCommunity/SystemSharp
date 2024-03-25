48b7e355f36b5818718435bdd2478e1f020917d6
CreeperGuy72192 <65190117+CreeperGuy72192@users.noreply.github.com>
2024-03-25 05:04:55 -0400

    Wjjejw
    W

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ dotnet Tools/Mosa.Tool.Launcher.Console.dll -launch-off -autostart -threading -o
 # -launch-off -output-asm -output-debug -output-hash
 cd ../../..
 
-# ISO Creation -- Temporary disabled for building speed/or until I can get Limine to build MOSA as an ISO (which someone removed because it was 'useless')
+# ISO Creation -- Temporary disabled for building speed/or until I can get Limine to build MOSA as an ISO
 # cp /var/folders/6g/cn2k0n6n6mxf063hwd89rbch0000gn/T/MOSA/System.x86.img ISO/CD_root/images
 # mv ISO/CD_root/images/System.x86.img ISO/CD_root/images/System.img
 # mkisofs -o ISO/System.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table ISO/CD_root
