94d24b5e33a8d0c179cb396a24f998e26fcdf2aa
CreeperGuy72192 <65190117+CreeperGuy72192@users.noreply.github.com>
2024-03-26 00:46:03 -0400

    Files

--- System/System.x86/obj/Debug/System.x86.AssemblyInfo.cs
+++ System/System.x86/obj/Debug/System.x86.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("System.x86")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22fe0bd44ec7c5dc01f20df31d522031460cdcde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8234bb60e32d5aa2095de0a527a3f99005e92cfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("System.x86")]
 [assembly: System.Reflection.AssemblyTitleAttribute("System.x86")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
